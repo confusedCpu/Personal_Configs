528d80ac0afdc0ad86768560f22f144ff045f16f
resnet <RAMS\resnet@cli64>
2017-06-02 16:23:29 -0400

    added to contacts :

--- python/contact/main.py
+++ python/contact/main.py
@@ -1,6 +1,32 @@
 #!/usr/bin/python3
-
 # This is a simple application to keep track of addresses and contact information from the cli
+
+import os
+
+def open_book(book_path, contacts[]): # opens/creates the contact book and loads it into the contacts list
+  if not os.path.isfile(book_path):
+    file(book_path, 'w').close()
+    print('New contact book ' + book.name + ' created.')
+
+  try:
+    book = open(book_path, 'r')
+  except OSError, err:
+      print('There was an issue opening the file. The program will now close')
+      sys.exit()
+
+  while not book.closed:
+    i = 0
+    for line in book:
+      contacts[i] = build_contact(book.readline())
+      i += 1
+    del i
+    book.close()
+        
+  print('The contact book has been loaded into memory.')
+
+def build_contact(line):
+  
+
 
 def menu(): # print the list of available actions and handle cli
   exit = False
@@ -15,7 +41,7 @@ def menu(): # print the list of available actions and handle cli
     elif response == '4': update_contact(); continue
     elif response == '5': delete_contact(); continue
     elif response == '6': exit = True; continue
-    else: print('That is not a valid response.\n\n')
+    else: print'That is not a valid response.\n\n'
   exit = None
   response = None
   del exit
@@ -26,12 +52,15 @@ def print_book():
 def search_book(): 
 
 def add_contact():
-
+  # templete for contact
+  # contact = {Name: '', Phone: '', Email: ''}
+  
 def update_contact():
 
 def delete_contact():
     
 def main():
   
+    open_book()
     menu()
 
